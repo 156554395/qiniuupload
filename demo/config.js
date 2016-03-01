7594eb19f3c6988fcef9d43d38284c7cf77237ba
seostar <seostart@qq.com>
2016-03-01 18:06:42 +0800

    配置文件

---
+++ demo/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': 'B9AM4tyv5tDjk4IWMl7_VG9KvDdYkh6IjuNEZT3S',
+    'SECRET_KEY': 'KqZyKo2sX83tj-vUm_xoEEHeWy0GS4BGAXdjaPnl',
+    'Bucket_Name': 'wangxiaobao',
+    'Port': 19110,
+    'Uptoken_Url': 'uptoken',
+    'Domain': 'http://7xr4ej.com1.z0.glb.clouddn.com/'
+};
